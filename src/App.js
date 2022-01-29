5bdf926ea136d7ff78d1dd0246a5bc9884a64f14
Ciel/howard <howardlight+github@protonmail.com>
2022-01-29 22:53:33 +0200

    Created API fetch for Profiles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  // init States
+  const [profile, setProfile] = useState("null");
+  const [profileKey, setProfileKey] = useState("defunkt");
+
+  // Fetch Item
+  useEffect(() => {
+    const baseUrl = "https://api.github.com/users/";
+    fetch(`${baseUrl}${profileKey}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response =>  {return response.json()} )
+    .then(data => {
+
+      console.log(data);
+      setProfile(data);
+    
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  },[profileKey]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {profile.name}
         </p>
         <a
           className="App-link"
