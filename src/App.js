05d88e36e457c7f8e8118ac4f0db9f89ac44ece9
Ciel/howard <howardlight+github@protonmail.com>
2022-02-01 21:47:32 +0200

    Ported repoCard to its own JS file

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ import {
 } from "@mui/material";
 
 // ICONS
-import StarIcon from '@mui/icons-material/Star';
-import ForkRightIcon from '@mui/icons-material/ForkRight';
 import WarningIcon from '@mui/icons-material/Warning';
+
+import repoCard from "./components/RepoCard";
 
 function App() {
 
@@ -164,32 +164,6 @@ function App() {
       </div>
     );
   }
-
-  function repoCard(repo) {
-    // console.log(repo["license"]);
-    //TODO: Center SVGs (Icons)
-    const date = new Date(repo.created_at);
-    return(
-      <CardActionArea key={repo.id} href={repo.html_url} target="_blank">
-        <Card style={{padding: "30px", marginTop: "10px", marginBottom:"10px", display:"flex"}}>
-          <Container style={{display: "flex", flexDirection: "column", justifyContent:"flex-start"}}>
-            <Typography variant="h6">{repo.name}</Typography>
-            <Typography style={{display: "flex", alignItems: "center"}}>Open Issues: {repo.open_issues}</Typography>
-            <Typography style={{display: "flex", alignItems: "center"}}>Is Licenced: {repo["license"] ? repo["license"].name : "False"}</Typography>
-            <Typography style={{display: "flex", alignItems: "center"}}>Created at: {date.toDateString()}</Typography>
-            <Typography style={{display: "flex", alignItems: "center"}}>Language used: {repo.language}</Typography>
-
-            <Container style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", paddingLeft: "0px", marginTop: "30px"}}>
-              <Typography style={{marginRight: "10px", display: "inline-flex", alignItems: "center"}}><StarIcon />{repo.stargazers_count}</Typography>
-              <Typography style={{marginRight: "10px", display: "inline-flex", alignItems: "center"}}><ForkRightIcon />{repo.forks}</Typography>
-            </Container>
-          </Container>
-          <Avatar variant="rounded" alt={"Profile IMG"} sx={{ width: 128, height: 128}}>{repo.name[0]}</Avatar>
-        </Card>
-      </CardActionArea>
-    );
-  }
-
 
   //TODO: create a good layout, maybe use a framework like bootstrap
   //TODO: Add animations
---
+++ src/components/RepoCard.js
@@ -0,0 +1,33 @@
+import {
+    Typography,
+    Card,
+    CardActionArea,
+    Container,
+    Avatar
+} from "@mui/material"
+
+import StarIcon from '@mui/icons-material/Star';
+import ForkRightIcon from '@mui/icons-material/ForkRight';
+
+export default function repoCard(repo) {
+    const date = new Date(repo.created_at);
+    return(
+      <CardActionArea key={repo.id} href={repo.html_url} target="_blank">
+        <Card style={{padding: "30px", marginTop: "10px", marginBottom:"10px", display:"flex"}}>
+          <Container style={{display: "flex", flexDirection: "column", justifyContent:"flex-start"}}>
+            <Typography variant="h6">{repo.name}</Typography>
+            <Typography style={{display: "flex", alignItems: "center"}}>Open Issues: {repo.open_issues}</Typography>
+            <Typography style={{display: "flex", alignItems: "center"}}>Is Licenced: {repo["license"] ? repo["license"].name : "False"}</Typography>
+            <Typography style={{display: "flex", alignItems: "center"}}>Created at: {date.toDateString()}</Typography>
+            <Typography style={{display: "flex", alignItems: "center"}}>Language used: {repo.language}</Typography>
+
+            <Container style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", paddingLeft: "0px", marginTop: "30px"}}>
+              <Typography style={{marginRight: "10px", display: "inline-flex", alignItems: "center"}}><StarIcon />{repo.stargazers_count}</Typography>
+              <Typography style={{marginRight: "10px", display: "inline-flex", alignItems: "center"}}><ForkRightIcon />{repo.forks}</Typography>
+            </Container>
+          </Container>
+          <Avatar variant="rounded" alt={"Profile IMG"} sx={{ width: 128, height: 128}}>{repo.name[0]}</Avatar>
+        </Card>
+      </CardActionArea>
+    );
+  }
