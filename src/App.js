446332ba53dd774986c3def125043aa59d9a1f36
Ciel/howard <howardlight+github@protonmail.com>
2022-02-01 20:42:23 +0200

    Added 404 card for when github acc is not found

--- src/App.js
+++ src/App.js
@@ -136,7 +136,20 @@ function App() {
     );  
   }
 
-  //TODO: error handling for when fetch yields 404
+  // TODO improve this
+  const Display404 = () => {
+    return(
+      <div className='ProfileCard'>
+        <Container style={{marginTop: "20px", marginBottom: "20px"}}>
+          <Paper elevation={10} style={{padding: "20px", paddingLeft:"20px", paddingRight: "20px", display:"flex"}}>
+            <Container style={{padding: "30px"}}>
+              <Typography variant="h5" style={{display: "inline-flex", alignItems: "center"}}><WarningIcon fontSize="large" color="warning"/>404: User not found</Typography>
+            </Container>
+          </Paper>
+        </Container>
+      </div>
+    );
+  }
   //TODO: create a good layout, maybe use a framework like bootstrap
   //TODO: Add animations
   //TODO: Create func that will get top 4 repos, use https://api.github.com/users/Howardlight/repos
