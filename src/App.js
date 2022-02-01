6405f76072a3c7b3ebb51a3ed85346dbd7f1ef80
Ciel/howard <howardlight+github@protonmail.com>
2022-02-01 20:42:46 +0200

    Created repoCard

--- src/App.js
+++ src/App.js
@@ -150,12 +150,40 @@ function App() {
       </div>
     );
   }
+
+  function repoCard(repo) {
+    // console.log(repo["license"]);
+    //TODO: Center SVGs (Icons)
+    const date = new Date(repo.created_at);
+    return(
+      <CardActionArea key={repo.id} href={repo.html_url} target="_blank">
+        <Card style={{padding: "30px", marginTop: "10px", marginBottom:"10px", display:"flex"}}>
+          <Container style={{display: "flex", flexDirection: "column", justifyContent:"flex-start"}}>
+            <Typography variant="h6">{repo.name}</Typography>
+            <Typography style={{display: "flex", alignItems: "center"}}>Open Issues: {repo.open_issues}</Typography>
+            <Typography style={{display: "flex", alignItems: "center"}}>Is Licenced: {repo["license"] ? repo["license"].name : "False"}</Typography>
+            <Typography style={{display: "flex", alignItems: "center"}}>Created at: {date.toDateString()}</Typography>
+            <Typography style={{display: "flex", alignItems: "center"}}>Language used: {repo.language}</Typography>
+
+            <Container style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", paddingLeft: "0px", marginTop: "30px"}}>
+              <Typography style={{marginRight: "10px", display: "inline-flex", alignItems: "center"}}><StarIcon />{repo.stargazers_count}</Typography>
+              <Typography style={{marginRight: "10px", display: "inline-flex", alignItems: "center"}}><ForkRightIcon />{repo.forks}</Typography>
+            </Container>
+          </Container>
+          <Avatar variant="rounded" alt={"Profile IMG"} sx={{ width: 128, height: 128}}>{repo.name[0]}</Avatar>
+        </Card>
+      </CardActionArea>
+    );
+  }
+
+
   //TODO: create a good layout, maybe use a framework like bootstrap
   //TODO: Add animations
   //TODO: Create func that will get top 4 repos, use https://api.github.com/users/Howardlight/repos
+  //TODO: Turn the 404 into a Component, use Material UI
+  //TODO: add light mode/ dark mode
   return (
     <Container className="App" maxWidth="sm">
-
       <Hero />
 
 
