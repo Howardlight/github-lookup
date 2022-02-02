84645aeee1a09fe66f6d78d778346a48b70de318
Ciel/howard <howardlight+github@protonmail.com>
2022-02-02 22:30:50 +0200

    Centered Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export default function Hero() {
     return(
-      <Typography className="hero" variant="h4" component="h1" gutterBottom ml={5}>
+      <Typography className="hero" variant="h4" component="h1" gutterBottom style={{paddingTop: "10vh", display: "flex", justifyContent: "center"}}>
         Github Profiles
       </Typography>
     );
