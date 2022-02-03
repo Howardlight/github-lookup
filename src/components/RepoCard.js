7d0767317ee3f25ac1e9820d2ce70639d06269d5
Ciel/howard <howardlight+github@protonmail.com>
2022-02-03 20:51:05 +0200

    Ported RepoCard Styles into a CSS module

--- src/components/RepoCard.js
+++ src/components/RepoCard.js
@@ -5,6 +5,7 @@ import {
     Container,
     Avatar
 } from "@mui/material"
+import styles from "./RepoCard.module.css";
 
 import StarIcon from '@mui/icons-material/Star';
 import ForkRightIcon from '@mui/icons-material/ForkRight';
@@ -13,17 +14,17 @@ export default function RepoCard(repo) {
     const date = new Date(repo.created_at);
     return(
       <CardActionArea key={repo.id} href={repo.html_url} target="_blank">
-        <Card style={{padding: "30px", marginTop: "10px", marginBottom:"10px", display:"flex"}}>
-          <Container style={{display: "flex", flexDirection: "column", justifyContent:"flex-start"}}>
+        <Card className={styles.card}>
+          <Container className={styles.mainContainer}>
             <Typography variant="h6">{repo.name}</Typography>
-            <Typography style={{display: "flex", alignItems: "center"}}>Open Issues: {repo.open_issues}</Typography>
-            <Typography style={{display: "flex", alignItems: "center"}}>Is Licenced: {repo["license"] ? repo["license"].name : "False"}</Typography>
-            <Typography style={{display: "flex", alignItems: "center"}}>Created at: {date.toDateString()}</Typography>
-            <Typography style={{display: "flex", alignItems: "center"}}>Language used: {repo.language}</Typography>
+            <Typography className={styles.profileTextInfo}>Open Issues: {repo.open_issues}</Typography>
+            <Typography className={styles.profileTextInfo}>Is Licenced: {repo["license"] ? repo["license"].name : "False"}</Typography>
+            <Typography className={styles.profileTextInfo}>Created at: {date.toDateString()}</Typography>
+            <Typography className={styles.profileTextInfo}>Language used: {repo.language}</Typography>
 
-            <Container style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", paddingLeft: "0px", marginTop: "30px"}}>
-              <Typography style={{marginRight: "10px", display: "inline-flex", alignItems: "center"}}><StarIcon />{repo.stargazers_count}</Typography>
-              <Typography style={{marginRight: "10px", display: "inline-flex", alignItems: "center"}}><ForkRightIcon />{repo.forks}</Typography>
+            <Container className={styles.secondaryContainer}>
+              <Typography className={styles.iconStats}><StarIcon />{repo.stargazers_count}</Typography>
+              <Typography className={styles.iconStats}><ForkRightIcon />{repo.forks}</Typography>
             </Container>
           </Container>
           <Avatar variant="rounded" alt={"Profile IMG"} sx={{ width: 128, height: 128}}>{repo.name[0]}</Avatar>
