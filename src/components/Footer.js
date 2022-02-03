457c1f93336dfea619f5a450e51af0ea17afb07c
Ciel/howard <howardlight+github@protonmail.com>
2022-02-03 20:06:40 +0200

    Added Padding to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export default function Footer() {
     return(
-      <Typography variant="body2" color="text.secondary" align="center">
+      <Typography variant="body2" color="text.secondary" style={{paddingTop: "5vh"}} align="center">
         Created by <a href="https://github.com/Howardlight" style={{textDecoration: "none"}}>HowardLight</a>, 2022
       </Typography>
     );
