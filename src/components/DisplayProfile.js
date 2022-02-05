448686fd4be22e31bf14d13ae6bf932be652e634
Ciel/howard <howardlight+github@protonmail.com>
2022-02-05 20:55:26 +0200

    Stringified profile creation date

--- src/components/DisplayProfile.js
+++ src/components/DisplayProfile.js
@@ -7,20 +7,21 @@ import {
 import styles from "./DisplayProfile.module.css";
 
 export function DisplayProfile({profile}) {
-    return(
-      <div className='ProfileCard'>
-        <Container className={styles.mainContainer}>
-          <Paper elevation={10} className={styles.paper}>
-            <Container className={styles.secondaryContainer}>
-              <Typography>Login Name: {profile.login}</Typography>
-              <Typography>Name: {profile.name}</Typography>
-              <Typography>Created at: {profile.created_at}</Typography>
-              <Typography>Followers: {profile.followers}</Typography>
-              <Typography>Repo count: {profile.public_repos}</Typography>
-            </Container>
-            <Avatar src={profile.avatar_url} variant="rounded" alt={"Profile IMG"} sx={{ width: 128, height: 128}}/>
-          </Paper>
-        </Container>
-      </div>
-    );  
+  const date = new Date(profile.created_at);
+  return(
+    <div className='ProfileCard'>
+      <Container className={styles.mainContainer}>
+        <Paper elevation={10} className={styles.paper}>
+          <Container className={styles.secondaryContainer}>
+            <Typography>Login Name: {profile.login}</Typography>
+            <Typography>Name: {profile.name}</Typography>
+            <Typography>Created at: {date.toDateString()}</Typography>
+            <Typography>Followers: {profile.followers}</Typography>
+            <Typography>Repo count: {profile.public_repos}</Typography>
+          </Container>
+          <Avatar src={profile.avatar_url} variant="rounded" alt={"Profile IMG"} sx={{ width: 128, height: 128}}/>
+        </Paper>
+      </Container>
+    </div>
+  );  
   }
