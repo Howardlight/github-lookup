54abe5d047f0878b5374ff99f68c2e47d161663c
Ciel/howard <howardlight+github@protonmail.com>
2022-02-08 12:50:07 +0200

    Improved CSS for Display404

--- src/components/Display404.js
+++ src/components/Display404.js
@@ -2,6 +2,7 @@ import {
     Container,
     Typography,
     Paper,
+    Grid,
 } from "@mui/material"
 import WarningIcon from '@mui/icons-material/Warning';
 import styles from "./Display404.module.css";
@@ -12,9 +13,18 @@ export default function Display404() {
   return(
     <div className='ProfileCard'>
       <Container className={styles.mainContainer}>
-        <Paper elevation={10} className={styles.paper}>
+        <Paper elevation={10} className={styles.paper} style={{alignItems:"center", justifyContent: "spacedEvenly"}}>
           <Container className={styles.secondaryContainer}>
-            <Typography variant="h5" className={styles.textContent}><WarningIcon fontSize="large" color="warning"/>404: User not found</Typography>
+            <Typography variant="h5" 
+            className={styles.textContent}
+            >
+              <WarningIcon 
+              fontSize="large" 
+              color="warning"
+              style={{marginRight: "7px"}}
+              />
+                404: User not found
+              </Typography>
           </Container>
         </Paper>
       </Container>
