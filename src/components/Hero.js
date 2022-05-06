2d781b4186697d061970553896814541dd822649
HowardLight <howardlight@protonmail.com>
2022-05-06 22:43:33 +0300

    Removed Flexbox Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export default function Hero() {
     return(
-      <Typography className="hero" variant="h4" component="h1" gutterBottom style={{paddingTop: "10vh", display: "flex", justifyContent: "center"}}>
+      <Typography className="hero" variant="h4" component="h1" gutterBottom>
         Github Profiles
       </Typography>
     );
