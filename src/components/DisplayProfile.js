72a0b34ad4eac5da650c11b9e0ca3c66a0931336
HowardLight <howardlight@protonmail.com>
2022-05-06 22:37:39 +0300

    Removed pointless container

--- src/components/DisplayProfile.js
+++ src/components/DisplayProfile.js
@@ -1,27 +1,23 @@
-import {
-    Typography,
-    Container,
-    Paper,
-    Avatar
-} from "@mui/material";
+import {Avatar, Container, Paper, Typography} from "@mui/material";
 import styles from "./DisplayProfile.module.css";
+import { Fragment } from "react";
 
 export function DisplayProfile({profile}) {
   const date = new Date(profile.created_at);
-  return(
-    <div className='ProfileCard'>
-      <Container className={styles.mainContainer}>
-        <Paper elevation={10} className={styles.paper}>
-          <Container className={styles.secondaryContainer}>
-            <Typography>Login Name: {profile.login}</Typography>
-            <Typography>Name: {profile.name}</Typography>
-            <Typography>Created at: {profile.created_at === undefined ? "" : date.toDateString()}</Typography>
-            <Typography>Followers: {profile.followers}</Typography>
-            <Typography>Repo count: {profile.public_repos}</Typography>
-          </Container>
-          <Avatar src={profile.avatar_url} variant="rounded" alt={"Profile IMG"} sx={{ width: 128, height: 128}}/>
-        </Paper>
-      </Container>
-    </div>
-  );  
-  }
+    return (
+        <Fragment>
+                <Paper elevation={10} className={styles.paper}>
+                    <Container className={styles.secondaryContainer}>
+                        <Typography>Login Name: {profile.login}</Typography>
+                        <Typography>Name: {profile.name}</Typography>
+                        <Typography>Created
+                            at: {profile.created_at === undefined ? "" : date.toDateString()}</Typography>
+                        <Typography>Followers: {profile.followers}</Typography>
+                        <Typography>Repo count: {profile.public_repos}</Typography>
+                    </Container>
+                    <Avatar src={profile.avatar_url} variant="rounded" alt={"Profile IMG"}
+                            sx={{width: 128, height: 128}}/>
+                </Paper>
+        </Fragment>
+    );
+}
