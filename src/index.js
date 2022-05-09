8ef75b44b0784b49a60301c0dc0aa935813d8f99
HowardLight <howardlight@protonmail.com>
2022-05-09 22:58:21 +0300

    Reversed Improved naming

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import * as ReactDOM from "react-dom/client";
 import './index.css';
-import {ToggleColorMode as App} from "./App";
+import ToggleColorMode from "./App";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <ToggleColorMode />
   </React.StrictMode>
 );
 
