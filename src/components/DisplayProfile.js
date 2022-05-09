f75f598435815bd357522e94f68f38916b12da57
HowardLight <howardlight@protonmail.com>
2022-05-09 22:40:35 +0300

    Replaced old fetch method with swr DisplayProfile

--- src/components/DisplayProfile.js
+++ src/components/DisplayProfile.js
@@ -1,9 +1,23 @@
-import {Avatar, Container, Paper, Typography} from "@mui/material";
+import {Avatar, CircularProgress, Container, Paper, Typography} from "@mui/material";
 import styles from "./DisplayProfile.module.css";
-import { Fragment } from "react";
+import { Fragment, Suspense, useEffect } from "react";
+import { useProfile } from "./Utils";
 
-export function DisplayProfile({profile}) {
-  const date = new Date(profile.created_at);
+export function DisplayProfile(
+    {profileName}
+    ) {
+    
+    const { profile, isLoading, isError } = useProfile(profileName);
+    // console.log(profileName);
+
+
+    if(isLoading) return <CircularProgress />
+    if(isError) {
+        console.log(isError)
+        return (<div>Error Occurred</div>);
+    }
+    
+    const date = new Date(profile.created_at);
     return (
         <Fragment>
                 <Paper elevation={10} className={styles.paper}>
@@ -11,7 +25,8 @@ export function DisplayProfile({profile}) {
                         <Typography>Login Name: {profile.login}</Typography>
                         <Typography>Name: {profile.name}</Typography>
                         <Typography>Created
-                            at: {profile.created_at === undefined ? "" : date.toDateString()}</Typography>
+                            at: {profile.created_at === undefined ? "" : date.toDateString()}
+                            </Typography>
                         <Typography>Followers: {profile.followers}</Typography>
                         <Typography>Repo count: {profile.public_repos}</Typography>
                     </Container>
