6a264a221ecadbad69f6657ec1f4e99c8d585f8c
HowardLight <howardlight@protonmail.com>
2022-05-09 22:55:04 +0300

    Improved naming in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import * as ReactDOM from "react-dom/client";
 import './index.css';
-import ToggleColorMode from "./App";
+import {ToggleColorMode as App} from "./App";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
-    <ToggleColorMode />
+    <App />
   </React.StrictMode>
 );
 
