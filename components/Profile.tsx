3892176724c4aaa5c2c77fe07e7d5dc563fd0ab2
MN <howardlight@protonmail.com>
2023-06-15 00:27:58 +0300

    Created ProfileNotFound component

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -6,7 +6,7 @@ import useSWR, { SWRResponse } from "swr";
 import { AxiosError } from "axios";
 import { GithubProfile } from "@/types";
 import Image from "next/image";
-
+import NotFound from "@/public/zoom-question.svg";
 
 export function Profile() {
     const query = useQueryStore((state) => state.query);
@@ -23,7 +23,7 @@ function ProfileComponent({ query }: { query: string }) {
     //TODO: Add Better Loading
 
     if (!data && !error) return <p>Loading...</p>;
-    if (error || data == undefined) return <p>Error Occured</p>
+    if (error || data == undefined) return <ProfileNotFound />;
     const date = new Date(data.created_at);
     return (
         <div className="flex flex-row gap-2 shadow-md ml-5 mr-5 pl-2 pr-2 pt-2 pb-2">
@@ -42,4 +42,21 @@ function ProfileComponent({ query }: { query: string }) {
             </div>
         </div>
     )
+}
+
+function ProfileNotFound() {
+    return (
+        <div className="flex flex-col justify-center items-center gap-5 mt-5">
+            <Image
+                src={NotFound}
+                alt="Not Found Image"
+                width={128}
+                height={128}
+            />
+            <div className="flex flex-col justify-center items-center">
+                <p className="font-semibold">Profile Not found</p>
+                <p className="text-sm">Please make sure the username is correct.</p>
+            </div>
+        </div>
+    );
 }
