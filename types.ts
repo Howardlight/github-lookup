d5cb39db4c4f5763097abd483fa02556380b7639
MN <howardlight@protonmail.com>
2023-06-15 00:10:46 +0300

    Added SvgIconConstituentValues

--- types.ts
+++ types.ts
@@ -141,3 +141,28 @@ export interface Owner {
     type: string;
     site_admin: boolean;
 }
+
+export interface SvgIconConstituentValues {
+    strokeColor?: string;
+    strokeWidth?: string;
+    strokeWidth2?: string;
+    strokeWidth3?: string;
+    strokeFill?: string;
+    fillColor?: string;
+    fillColor2?: string;
+    fillColor3?: string;
+    fillColor4?: string;
+    fillColor5?: string;
+    fillColor6?: string;
+    fillColor7?: string;
+    imageWidth?: string;
+    imageHeight?: string;
+    width?: string;
+    height?: string;
+    rotateCenter?: number;
+    className?: string;
+    className2?: string;
+    className3?: string;
+    className4?: string;
+    className5?: string;
+}
